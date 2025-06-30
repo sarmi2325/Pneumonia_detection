eb78a9780a140d7296b7611769452c368cd103a9
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2025-06-30 09:51:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from tensorflow.keras.models import load_model
 from utils import preprocess_image, make_gradcam_heatmap, superimpose_heatmap
 
 # Load models
-model_cnn = load_model("cnn_v3.keras")
+#model_cnn = load_model("cnn_v3.keras")
 model_mobilenet = load_model("mobilenetv2.keras")
+model_cnn = load_model("cnn_v3.keras")
 model_effnet = load_model("efficientnetb0_v3.keras")
 
 models = {
