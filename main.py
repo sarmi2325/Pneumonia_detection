4af4fbbe0594531e37973e5086eecab1d06e3073
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2025-07-16 22:47:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,13 +26,13 @@ class_names = ['NORMAL', 'PNEUMONIA']
 st.markdown(
     """
     <div style='text-align:center; padding: 10px 0;'>
-        <h1 style='color:#3C3C3C;'>🧠 Pneumonia Detection using Deep Learning Ensemble</h1>
+        <h1> Pneumonia Detection using Deep Learning Ensemble</h1>
         <p style='font-size:18px;'>Upload a chest X-ray to get predictions from 3 models and interpret it using Grad-CAM visualization.</p>
     </div>
     """, unsafe_allow_html=True
 )
 
-uploaded_file = st.file_uploader("📤 Upload a Chest X-ray Image", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Upload a Chest X-ray Image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
@@ -42,12 +42,12 @@ if uploaded_file:
     
     col1, col2 = st.columns([1, 1])
     with col1:
-        st.image(image.resize((400, 400)), caption="🖼️ Original Chest X-ray", use_container_width=True)
+        st.image(image.resize((400, 400)), caption="Original Chest X-ray", use_container_width=True)
     with col2:
         try:
             heatmap = make_gradcam_heatmap(img_array, model_effnet, last_conv_layer_name="top_conv")
             cam_image = superimpose_heatmap(heatmap, image)
-            st.image(Image.fromarray(cam_image).resize((400, 400)), caption="🔥 Grad-CAM (EfficientNetB0)", use_container_width=True)
+            st.image(Image.fromarray(cam_image).resize((400, 400)), caption="Grad-CAM (EfficientNetB0)", use_container_width=True)
         except Exception as e:
             st.error(f"Grad-CAM error: {e}")
 
@@ -88,7 +88,7 @@ if uploaded_file:
         f"""
         <div style='background-color: #e6f0ff; padding: 25px; border-radius: 15px; border: 2px solid #99ccff;
                     text-align: center; margin: auto; width: 70%; box-shadow: 3px 3px 10px rgba(0,0,0,0.1);'>
-            <h2>🧠 Final Ensemble Prediction: 
+            <h2>Final Ensemble Prediction: 
                 <span style='color: {"red" if final_class == "PNEUMONIA" else "green"};'>{final_class}</span></h2>
             <p style='font-size: 18px;'>🔴 PNEUMONIA Probability: <strong>{weighted_avg_prob * 100:.2f}%</strong></p>
             <p style='font-size: 18px;'>🟢 NORMAL Probability: <strong>{(1 - weighted_avg_prob) * 100:.2f}%</strong></p>
@@ -100,7 +100,7 @@ if uploaded_file:
     st.markdown("<br><br>", unsafe_allow_html=True)
 
     # Optional: Expandable section
-    with st.expander("🧠 How the Ensemble Works"):
+    with st.expander("How the Ensemble Works"):
         st.write("""
         Each model outputs a probability score for the image. Instead of averaging directly,
         we assign **higher weight to models that are more confident**, using a dynamic confidence-based weighted voting system.
